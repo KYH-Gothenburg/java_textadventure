c868651fb9e853d8ea329bcb47e983481b226a6b
Joakim Wassberg <joakim@arthead.se>
2021-09-21 10:47:09 +0200

    Commit 1 21 sept

--- src/kyh/textadventure/Main.java
+++ src/kyh/textadventure/Main.java
@@ -5,20 +5,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String[][] map = {
+                {"A pink room", "A hall"},
+                {"The entrance", "A dark cave"}
+        };
+        int row = 1;
+        int col = 0;
         Scanner input = new Scanner(System.in);
 
         System.out.println("Welcome to the Text Adventure Game (TAG)");
 
         boolean running = true;
         while(running) {
-            System.out.println("You are standing in a dark room. You can hear bats flying above you");
+            System.out.println(map[row][col]);
 
             System.out.print("> ");
             String command = input.nextLine();
+
             String[] commandParts = command.split(" ");
+
             if(commandParts[0].equalsIgnoreCase("go")) {
                 // Kolla efter riktning
                 if(commandParts.length >= 2) {
+                    if(commandParts[1].equalsIgnoreCase("north")) {
+                        row--;
+                        if(row < 0) {
+                            row = 0;
+                        }
+                    }
                     System.out.println("Going " + commandParts[1]);
                 }
                 else {
