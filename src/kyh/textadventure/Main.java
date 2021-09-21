b2438468b661800a65d793b7af59ed849f52b4df
Joakim Wassberg <joakim@arthead.se>
2021-09-21 12:53:17 +0200

    Commit 2 21 sept

--- src/kyh/textadventure/Main.java
+++ src/kyh/textadventure/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Initialisering
         String[][] map = {
                 {"A pink room", "A hall"},
                 {"The entrance", "A dark cave"}
@@ -16,21 +17,54 @@ public class Main {
         System.out.println("Welcome to the Text Adventure Game (TAG)");
 
         boolean running = true;
+
+        // Här börjar spelloopen
         while(running) {
+            // 1. Skriv ut i vilket rum vi är i
             System.out.println(map[row][col]);
 
+            // 2. Läs in kommando från användaren
             System.out.print("> ");
             String command = input.nextLine();
 
+            // 3. Dela upp kommandot i delar, varje ord blir en sträng i en array
+            //    Vi delar upp det inmatade värdet vid varje mellanslag
             String[] commandParts = command.split(" ");
 
+            // 4. Kollar vilket "huvudkommando" som angivits
+            //    Dessa är:
+            //      - go
+            //      - quit
             if(commandParts[0].equalsIgnoreCase("go")) {
-                // Kolla efter riktning
+                // Vi har angett go som kommando
+
+
+                // Kontrollera att man har skrivit något efter go, alltså en riktning
                 if(commandParts.length >= 2) {
+                    // Kolla efter riktning
                     if(commandParts[1].equalsIgnoreCase("north")) {
                         row--;
+                        // Kontrollera så vi inte hamnar utanför kartan
                         if(row < 0) {
                             row = 0;
+                        }
+                    }
+                    else if(commandParts[1].equalsIgnoreCase("south")) {
+                        row++;
+                        if(row >= map.length) {
+                            row--;
+                        }
+                    }
+                    else if(commandParts[1].equalsIgnoreCase("east")) {
+                        col++;
+                        if(col >= map[row].length) {
+                            col--;
+                        }
+                    }
+                    else if(commandParts[1].equalsIgnoreCase("west")) {
+                        col--;
+                        if(col < 0) {
+                            col = 0;
                         }
                     }
                     System.out.println("Going " + commandParts[1]);
